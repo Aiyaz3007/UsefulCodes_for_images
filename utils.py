97da2621dd280f88f47a46a874336e95843db448
Aiyaz3007 <maiyaz3007@gmail.com>
2023-04-04 07:32:48 +0530

    added resize annotation function

--- utils.py
+++ utils.py
@@ -1,46 +1,29 @@
 import numpy as np
 import cv2
+  
+def get_image_info(annotationDict:dict,image_id:int):
+    return [[ann["category_id"],ann["bbox"]] for ann in annotationDict["annotations"] if ann["image_id"] == image_id]
 
+def cv2_draw(image_data:list,bbox:list,text:str,color:tuple=(0,0,255),thickness:int=2,display:bool=True):
+    bbox = (list(map(int,bbox)))
+    cv2.rectangle(img=image_data,pt1=(bbox[:2]),pt2=(np.array(bbox[2:]).__add__(bbox[:2])),color=color,thickness=thickness)
+    cv2.putText(img=image_data,text=text,org=np.array(bbox[:2]).__add__([0,-10]),color=color,fontFace=cv2.FONT_ITALIC,fontScale=2,thickness=2)
+    if display:
+        cv2.imshow("Frame",image_data)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+       return image_data
 
-def get_xml_coorinates(filename:str):
-  def readFile(filename:str):
-    text = ""
-    with open(filename,"r") as f:
-      text = f.read()
-    return text
+def resize_annotations(image_dim:tuple,bbox:list,resize:tuple):
+    """resize annotations
+    Args:
+        image_dim (tuple): image shape
+        bbox (list): bbox coordinates [x,y,w,h]
+        resize (tuple): resize dimension (x,y)
 
-  data = readFile(filename)
-  filename = data[data.find("<filename>"):data.find("</filename>")].replace("<filename>","")
-  category = data[data.find("<name>"):data.find("</name>")].replace("<name>","")
-
-  xmin = int(data[data.find("<xmin>"):data.find("</xmin>")].replace("<xmin>",""))
-  ymin = int(data[data.find("<ymin>"):data.find("</ymin>")].replace("<ymin>",""))
-  xmax = int(data[data.find("<xmax>"):data.find("</xmax>")].replace("<xmax>",""))
-  ymax = int(data[data.find("<ymax>"):data.find("</ymax>")].replace("<ymax>",""))
-  return filename,category,(xmin,ymin,xmax,ymax)
-
-
-def resize_image_annotations(filename:str,resize:tuple):
-  imageToPredict = cv2.imread(filename)
-  x_ = imageToPredict.shape[0]
-  y_ = imageToPredict.shape[1]
-
-  x_scale = resize[0]/x_
-  y_scale = resize[1]/y_
-  img = cv2.resize(imageToPredict,(416,416))
-  img = np.array(img)
-
-
-  x = int(np.round(128*x_scale))
-  y = int(np.round(25*y_scale))
-  xmax= int(np.round  (447*(x_scale)))
-  ymax= int(np.round(375*y_scale))
-
-  return (x,y,xmax,ymax)
-
-
-def draw_bbox(filename:str,coordinates:tuple,category:str):
-  image_data = cv2.imread(filename)
-  cv2.rectangle(image_data,(int(coordinates[0]),int(coordinates[1])),(int(coordinates[2]),int(coordinates[3])),(0,255,0),2)
-  cv2.putText(image_data,category,(int(coordinates[0]),int(coordinates[1])-10),cv2.FONT_HERSHEY_COMPLEX,2,(0,0,255))
-  return image_data
+    Returns:
+        list: [x,y,w,h]
+    """
+    width_ratio,height_ratio = [resize[idx]/image_dim[idx] for idx in range(len(resize))]
+    return [bbox[0]*height_ratio,bbox[1]*width_ratio,bbox[2]*height_ratio,bbox[3]*width_ratio]
