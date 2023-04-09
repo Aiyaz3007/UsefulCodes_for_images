0371b79faf9c58a498b5e8ea78deda019226f11c
Aiyaz3007 <maiyaz3007@gmail.com>
2023-04-09 07:12:04 +0530

    modified function and added resize arguments

--- custom_dataset_OD.py
+++ custom_dataset_OD.py
@@ -6,10 +6,12 @@ import cv2
 
 
 class CustomDataset():
-  def __init__(self,annotationFile:str,root_dir:str):
+  def __init__(self,annotationFile:str,root_dir:str,resize:tuple=None):
     self.coco = COCO(annotationFile)
     self.root_images = root_dir
     self.x_train = self.coco.getImgIds()
+    self.need_resize = False if resize == None else True
+    self.resize_size = resize
   def readFile(self,annotationfile:str):
     with open(annotationfile,"r") as f:
       return json.load(f)
@@ -33,7 +35,10 @@ class CustomDataset():
   def __getitem__(self,index):
     img_info = self.coco.loadImgs(self.x_train[index])[0]
     img_main = cv2.imread(os.path.join(self.root_images, img_info['file_name']))
-    img = cv2.resize(img_main,(600,600))
+    if self.need_resize:
+        img = cv2.resize(img_main,(600,600))
+    else:
+       img = img_main
     img = img/255  
     annIds = self.coco.getAnnIds(imgIds=img_info['id']) 
     anns = self.coco.loadAnns(annIds)
@@ -43,7 +48,8 @@ class CustomDataset():
 
     for i in range(num_objs):
         [x,y,w,h]=anns[i]["bbox"]
-        [x,y,w,h] = self.get_resized_annotations(img_main.shape,[x,y,w,h],(600,600))
+        if self.need_resize:
+            [x,y,w,h] = self.get_resized_annotations(img_main.shape,[x,y,w,h],self.resize_size)
         boxes[i] = torch.tensor(list(map(int,(x,y,x+w,y+h))))
         labels[i] = torch.tensor([anns[i]["category_id"]])
 
